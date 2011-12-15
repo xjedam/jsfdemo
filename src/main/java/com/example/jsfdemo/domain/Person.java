df26f692fb34a216279862251d86716a8d5a67ff
Ryszard Madejski <xjedam@gmail.com>
2011-12-15 15:05:38 +0100

    first commit

---
+++ src/main/java/com/example/jsfdemo/domain/Person.java
@@ -0,0 +1,41 @@
+package com.example.jsfdemo.domain;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class Person {
+	
+	private String name = "unknown";
+	private String email = "unknown@unkno.wn";
+	private int yob = 1900;
+	
+	@Size(min=3, max=30)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Min(value=1900)
+	public int getYob() {	
+		return yob;
+	}
+	public void setYob(int yob) {
+		this.yob = yob;
+	}
+	@Pattern(regexp="^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message="Not a valid email")
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
---
+++ src/main/java/com/example/jsfdemo/service/PersonManager.java
@@ -0,0 +1,50 @@
+package com.example.jsfdemo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import com.example.jsfdemo.domain.Person;
+
+@ApplicationScoped
+public class PersonManager {
+	private List<Person> db = new ArrayList<Person>();
+
+	public void addPerson(Person person) {
+		Person newPerson = new Person();
+		newPerson.setName(person.getName());
+		newPerson.setYob(person.getYob());
+		newPerson.setEmail(person.getEmail());
+		db.add(newPerson);
+	}
+
+	public void editPerson(Person person){
+		
+	}
+	public void updatePerson(Person person){
+
+		
+	}
+	// Removes the first person with given name and yob
+	public void deletePerson(Person person) {
+
+		Person personToRemove = null;
+
+		for (Person p : db) {
+			if (person.getName().equals(p.getName())
+					&& person.getYob() == p.getYob()
+					&& person.getEmail() == p.getEmail()) {
+				personToRemove = p;
+				break;
+			}
+		}
+
+		if (personToRemove != null)
+			db.remove(personToRemove);
+	}
+
+	public List<Person> getAllPerson() {
+		return db;
+	}
+}
